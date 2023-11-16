f76d9621fdd4cabd238e1f65679d64797d30312b
Salmauva <135599070+Salmauva@users.noreply.github.com>
2023-11-16 10:43:24 +0100

    making sin,tan as subcommandos

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,26 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
 
+    pass
+@cmd_group.command()
+@click.argument("number")
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
-    return
+    # return
 
-
+@cmd_group.command()
+@click.argument("number")
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-    return
+    # return
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group(10)
