6fd4d756d2422d39a851dfdb749cb2ca445a5ee4
Salmauva <135599070+Salmauva@users.noreply.github.com>
2023-11-16 11:15:32 +0100

    making option for number of steps

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,28 +1,48 @@
+# Salma Abd Adin
+# assignment about clicks
+
+# import needed libraries
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+
+# make group of multible sub commdandos
 @click.group()
 def cmd_group():
-
     pass
+
+
+# sub commando for sin with right steps
 @cmd_group.command()
-@click.argument("number")
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@click.option(
+    "-n",
+    "--count",
+    default=1,
+    help="Choose how many steps between 0 and 2pi you want",
+    show_default=True,  # show default in help
+)
+def sin(count):
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
-    # return
 
+
+# sub commando for tan with right steps
 @cmd_group.command()
-@click.argument("number")
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@click.option(
+    "-n",
+    "--count",
+    default=1,
+    help="Choose how many steps between 0 and 2pi you want",
+    show_default=True,  # show default in help
+)
+def tan(count):
+    x = np.linspace(0, 2 * pi, count)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-    # return
 
 
 if __name__ == "__main__":
-    cmd_group(10)
+    cmd_group()
